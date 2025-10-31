907a2fd711b48d0276b2371582542c2cc9982a73
Luciano Fagioli <lbfagioli@miuandes.cl>
2025-10-30 22:55:59 -0300

    feat: inital OpenGL project and physics demo

---
+++ main.cpp
@@ -0,0 +1,130 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include "shader_s.h"
+#include "fshader.h"
+#include "vshader.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+const int SCR_WIDTH = 800;
+const int SCR_HEIGHT = 600;
+const float g = -9.8f;
+const float jump_power = 8.0f;
+
+int WinMain()
+{
+	float acceleration = 0.0f;
+	float velocityY = 0.0f;
+	float positionY = 0.0f;
+	float currentTime = 0.0f;
+	float lastTime = 0.0f;
+	float deltaTime = 0.0f;
+	
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "triangulo jugueton que cae", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "failed to create glfw window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "failed to load glad (opengl)" << std::endl;
+		return -1;
+	}
+
+	Shader shaderProgram(vertexSource, fragmentSource);
+
+	// tricolor triangle
+	float vertices[] = {
+		//   vertices   |   colors       //
+		0.0f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+		0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f
+	};
+
+	unsigned int VAO, VBO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	lastTime = glfwGetTime();
+
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		shaderProgram.use();
+
+		float time = glfwGetTime();
+		float fadeRate = (sin(time*2)*sin(time*2) / 2.0f) + 0.5f;
+		shaderProgram.setFloat("fading", fadeRate);
+		shaderProgram.setFloat("rotationIlusion", sin(time*3));
+
+		currentTime = time;
+		deltaTime = currentTime - lastTime;
+
+		velocityY += (acceleration + g) * deltaTime;
+		positionY += (velocityY * deltaTime) / 5;
+		if (positionY < -0.5f)
+		{
+			velocityY = 0.0f;
+		}
+		if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+		{
+			velocityY = jump_power;
+		}
+		shaderProgram.setFloat("verticalOffset", positionY);
+		lastTime = currentTime;
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
